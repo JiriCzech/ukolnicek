1b9208bafb253b4280a901044d3ed17371e7fabc
JiriCzech <48186059+JiriCzech@users.noreply.github.com>
2021-11-27 20:12:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var tasks = document.querySelectorAll("li")
+var ul = document.querySelector("ul");
+
+function newTask(){
+    var text = prompt("Nový úlol:")
+    if (text !== ""){
+        var li = document.createElement("li")
+    li.innerHTML = text
+    li.addEventListener("click", ()=>{
+        li.classList.toggle("done")
+    })
+
+    ul.appendChild(li)
+    }
+    
+
+}
+
+
+tasks.forEach(element => {
+    element.addEventListener("click", ()=>{
+        element.classList.toggle("done")
+    })
+});
