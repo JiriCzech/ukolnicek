184445224b664585e923a784f9eb521a25ab90fd
JiriCzech <jirizlamalik@icloud.com>
2021-12-04 22:55:28 +0100

    Big upadte
    new functionality delete button

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
+// VARIABLES
 var tasks = document.querySelectorAll("li")
 var ul = document.querySelector("ul");
 
+
+
+//-----------------------------------------------------
+
+// FUNCTIONS
+// Create button
+function createDeleteButton(li){
+ var deleteButton = document.createElement("button")
+ deleteButton.innerText = "smazat"
+ deleteButton.classList.add("hide")
+ li.appendChild(deleteButton)
+
+ deleteButton.addEventListener("click", ()=>{
+     ul.removeChild(li)
+ })
+
+return deleteButton;
+
+
+ 
+
+}
+
+//---------------------------------------------------
+
+
+
+// Create new task
 function newTask(){
-    var text = prompt("Nový úlol:")
-    if (text != "" && text != null){
-        var li = document.createElement("li")
+
+    var text = prompt("Nový úkol:")// PopUp with new task name
+    if (text != "" && text != null){ // check if the task is not empty
+
+    var li = document.createElement("li") // Create new list item with specific task
     li.innerHTML = text
-    li.addEventListener("click", ()=>{
-        li.classList.toggle("done")
+    var deleteButton = createDeleteButton(li)
+
+    li.addEventListener("click", ()=>{ // add done line  if click to task
+    li.classList.toggle("done");
+    deleteButton.classList.toggle("hide");
+    
+    
     })
 
-    ul.appendChild(li)
+    ul.appendChild(li) 
     }
 
 
 }
 
 
-
+// Setings ->  
 
 tasks.forEach(element => {
     element.addEventListener("click", ()=>{
