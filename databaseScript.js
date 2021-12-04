862b55a9f5c0b16d8f87ef03c65c2447fdd17b71
JiriCzech <jirizlamalik@icloud.com>
2021-12-04 23:47:13 +0100

    database
    start coding firebae

---
+++ databaseScript.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVS_4ZUDRGYsaJ-St4ET3hdk41c9CQneA",
+    authDomain: "ukolnicek-5a4dd.firebaseapp.com",
+    projectId: "ukolnicek-5a4dd",
+    storageBucket: "ukolnicek-5a4dd.appspot.com",
+    messagingSenderId: "50402552517",
+    appId: "1:50402552517:web:f31f6c46fc75a247b19eac"
+  };
+
+  
--- script.js
+++ script.js
@@ -2,30 +2,62 @@
 var tasks = document.querySelectorAll("li")
 var ul = document.querySelector("ul");
 
-
+var tasks = [
+    {task: "umyt nadobi", isDone: true},
+    {task: "jit do obchodu", isDone: false},
+    {task: "spat", isDone: false},
+]
 
 //-----------------------------------------------------
 
 // FUNCTIONS
+
+//Create new list item
+function createNewLi(text,isDone){
+
+    // create new li
+    var li = document.createElement("li") // Create new list item with specific task
+    li.innerHTML = text
+    if (isDone == true){
+    li.classList.add("done");
+    }
+    var deleteButton = createDeleteButton(li, isDone)
+         // add click lisener
+    li.addEventListener("click", ()=>{ // add done line  if click to task
+    li.classList.toggle("done");
+    deleteButton.classList.toggle("hide");
+    })
+        
+    ul.appendChild(li) 
+            
+}
+
+
 // Create button
-function createDeleteButton(li){
+function createDeleteButton(li,isDone){
  var deleteButton = document.createElement("button")
  deleteButton.innerText = "smazat"
- deleteButton.classList.add("hide")
+ if (isDone == false){
+     deleteButton.classList.add("hide")
+ }
  li.appendChild(deleteButton)
 
  deleteButton.addEventListener("click", ()=>{
      ul.removeChild(li)
  })
-
 return deleteButton;
-
-
- 
-
 }
 
 //---------------------------------------------------
+
+
+// Main funkcionality
+
+// Add all task from Array
+tasks.forEach(task =>{
+    createNewLi(task.task, task.isDone)
+    })
+    
 
 
 
@@ -33,30 +65,9 @@ return deleteButton;
 function newTask(){
 
     var text = prompt("Nový úkol:")// PopUp with new task name
-    if (text != "" && text != null){ // check if the task is not empty
-
-    var li = document.createElement("li") // Create new list item with specific task
-    li.innerHTML = text
-    var deleteButton = createDeleteButton(li)
-
-    li.addEventListener("click", ()=>{ // add done line  if click to task
-    li.classList.toggle("done");
-    deleteButton.classList.toggle("hide");
-    
-    
-    })
-
-    ul.appendChild(li) 
+    if (text != "" && text != null){
+        createNewLi(text, false)
     }
-
-
 }
 
 
-// Setings ->  
-
-tasks.forEach(element => {
-    element.addEventListener("click", ()=>{
-        element.classList.toggle("done")
-    })
-});
